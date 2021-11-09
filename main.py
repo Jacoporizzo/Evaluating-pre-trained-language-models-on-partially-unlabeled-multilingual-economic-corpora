b453d80f88a4409180f257d74a21d89ed7e27193
Jacopo Rizzo <jacoporizzo@gmail.com>
2021-11-09 11:44:18 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# First way of setting up the df 
+# import os
+# os.chdir('C:/Users/Jacopo/Desktop/MA')
+from utils.imports import Import
+from utils.translations import Translation
+from utils.labels import Label
+from datetime import datetime
+
+# Get execution time
+start = datetime.now()
+
+# Import german labeled ad-hoc and their english counterpart 
+imp = Import()
+data = imp.findcounterpart()
+
+# Extract split doc variables for testing
+english_docs = data['bodyText_x'][0:10]
+german_docs = data['bodyText_y'][0:10]
+
+# Compute (cosine) similarity between translated sentences
+trans = Translation()
+cosine_scores = trans.cosine_similarity(english_docs, german_docs)
+
+# Import the goldstandards
+goldstd = imp.importgold()
+
+# Compute the english labeled dataframe basing on the cosine similarity outcome
+label = Label()
+english_labeled = label.merge_labels(data, goldstd, cosine_scores)
+
+print(datetime.now() - start)

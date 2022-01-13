58fe7dcf3d5ac71067c0806a853190865ea7910d
Jacopo Rizzo <jacoporizzo@gmail.com>
2022-01-13 17:56:56 +0100

    first bert finetuning

---
+++ various/finetune_bert.py
@@ -0,0 +1,61 @@
+'''
+Here we fine-tune the BERT model, which we will
+use to do the transfer-learning on the 8k-forms.
+This script can also be used to fine-tune any 
+other model, that is present on the huggingface
+hub.
+'''
+import pickle
+import numpy as np
+from datasets import Dataset, load_metric
+from transformers import (AutoModelForSequenceClassification,
+                          AutoTokenizer,
+                          TrainingArguments,
+                          Trainer)
+
+# Import data
+data = pickle.load(open('data/df_finetune.pkl', 'rb'))
+
+# Split dataset into train and test and conversion 
+# to right format. Dataset split: train 80%, test 20%
+df = Dataset.from_pandas(data).train_test_split(0.2, 0.8)
+
+# Model's name and paramaters and metrics
+model_name = 'bert-base-cased'
+metric = load_metric('accuracy')
+training_args = TrainingArguments(
+    output_dir = '../results',
+    evaluation_strategy = 'epoch',
+    learning_rate = 4e-5,
+    per_device_train_batch_size = 32,
+    per_device_eval_batch_size = 32,
+    num_train_epochs = 12,
+    weight_decay = 0.01,
+)
+
+# Tokenization
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+def tokenize_function(input):
+    return tokenizer(input['Text'], padding = 'max_length', truncation = True)
+
+tokenized_df = df.map(tokenize_function, batched = True)
+
+# Define evaluation metric to control during training
+def compute_metric(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+# Initialize model and start trainig (i.e. finetuning)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels = 22)
+trainer = Trainer(
+    model = model,
+    args = training_args,
+    train_dataset = tokenized_df["train"],
+    eval_dataset = tokenized_df["test"],
+    tokenizer = tokenizer,
+    compute_metrics = compute_metric
+)
+
+trainer.train()

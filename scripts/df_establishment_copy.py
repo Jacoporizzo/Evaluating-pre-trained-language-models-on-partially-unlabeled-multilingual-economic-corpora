d6225af4f379eae8670cf07946c61c5e3846d39d
Jacopo Rizzo <jacoporizzo@gmail.com>
2022-01-20 16:20:50 +0100

    rename folder

---
+++ scripts/df_establishment_copy.py
@@ -0,0 +1,34 @@
+'''
+Data preprocessing steps in order to set up the final 
+df to use. This script has been executed using a GPU
+and the results stored locally as pickle files. Adjust 
+the path of the data location before running the script. 
+'''
+
+from utils.imports import Import
+from utils.translations_copy import Translation
+from utils.labels import Label
+from datetime import datetime
+
+# Get execution time
+start = datetime.now()
+
+# Import german labeled ad-hoc and their english counterpart and goldstandards
+imp = Import()
+data = imp.findcounterpart()
+goldstandards = imp.importgold()
+
+# Compute (cosine) similarity between goldstandards and english sentences
+trans = Translation()
+cosine_scores = trans.cosine_similarity_test(data, goldstandards)
+
+# Compute the english labeled dataframe basing on the cosine similarity outcome
+label = Label()
+english_labelled = label.merge_labels(cosine_scores, goldstandards)
+
+# Save dfs in data folder
+cosine_scores.to_pickle('data/cosine_scores_pairs.pkl')
+english_labelled.to_pickle('data/english_goldstandards_pairs.pkl')
+
+# Print total computation time
+print(datetime.now() - start)

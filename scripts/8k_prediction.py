118e3da50a0fe0a720e57315f2f82c49cb80bc56
Jacopo Rizzo <jacoporizzo@gmail.com>
2022-02-23 18:50:39 +0100

    debugs

--- scripts/8k_prediction.py
+++ scripts/8k_prediction.py
@@ -7,6 +7,7 @@ import nltk
 nltk.download('punkt')
 import nltk.data
 import pandas as pd
+import numpy as np
 from datasets import Dataset
 from transformers import (AutoModelForSequenceClassification,
                           AutoTokenizer,
@@ -33,10 +34,25 @@ for form in range(len(df)):
 # Create df, remove rows with sentenece length == 1 and save as Dataset
 df_sent_split = pd.DataFrame({'text': sents, 'label': lab_items})
 df_sent_split = df_sent_split[df_sent_split['text'].str.split().str.len().ge(2)]
+df_sent_split = df_sent_split.reset_index(drop = True)
 df_split = Dataset.from_pandas(df_sent_split)
 
 with open('data/split_8k.pkl','wb') as fp:
     pickle.dump(df_split, fp)
+
+# Remove documents with item 7 and 8 for the first step and save. Removed
+# according to index of labels_names (from Helper class).
+idx_label = []
+for i in df_sent_split['label']:
+    idx_label.append(np.where(i == 1)[0][0])
+
+df_sent_split['idx_label'] = idx_label
+df_wo78 = df_sent_split[(df_sent_split['label'] != 6) & (df_sent_split['label'] != 17)]
+df_wo78 = df_wo78.reset_index(drop = True)
+df_inf = Dataset.from_pandas(df_wo78)
+
+with open('data/split_8k_wo78.pkl', 'wb') as fp:
+    pickle.dump(df_inf, fp)
 
 # Load model and tokenizer
 model_name = 'bert-base-cased'
@@ -47,7 +63,7 @@ model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
 # Make predictions with pipeline
 classifier = pipeline('text-classification', model = model, return_all_scores = True, tokenizer = tokenizer, function_to_apply = 'sigmoid')
-prediction = classifier(df_split['text'])
+prediction = classifier(df_inf['text'])
 
 # Save predictions
 with open('predictions_8k_v5.pkl', 'wb') as fp:

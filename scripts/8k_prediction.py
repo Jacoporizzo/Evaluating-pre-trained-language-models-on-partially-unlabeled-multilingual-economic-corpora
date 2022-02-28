ca75a711f7760e0d0aeba0e25fccb7e36654f024
Jacopo Rizzo <jacoporizzo@gmail.com>
2022-02-28 10:49:01 +0100

    pred bug fixes

--- scripts/8k_prediction.py
+++ scripts/8k_prediction.py
@@ -37,7 +37,7 @@ df_sent_split = df_sent_split[df_sent_split['text'].str.split().str.len().ge(2)]
 df_sent_split = df_sent_split.reset_index(drop = True)
 df_split = Dataset.from_pandas(df_sent_split)
 
-with open('data/split_8k.pkl','wb') as fp:
+with open('data/splitted_8k.pkl','wb') as fp:
     pickle.dump(df_split, fp)
 
 # Remove documents with item 7 and 8 for the first step and save. Removed
@@ -51,8 +51,20 @@ df_wo78 = df_sent_split[(df_sent_split['label'] != 6) & (df_sent_split['label'] 
 df_wo78 = df_wo78.reset_index(drop = True)
 df_inf = Dataset.from_pandas(df_wo78)
 
-with open('data/split_8k_wo78.pkl', 'wb') as fp:
+with open('data/8k_wo78.pkl', 'wb') as fp:
     pickle.dump(df_inf, fp)
+
+# Divide dataset in 10 equal parts and pass them to model for predictions
+df_wo78_split = np.array_split(df_wo78, 10)
+with open('data/8k_df_splits.pkl', 'wb'):
+    pickle.dump(df_wo78_split, fp)
+
+nr = 1
+for df in df_wo78_split:
+    name = 'split_part_' + str(nr) + '.pkl'
+    with open('data/8k_splits/' + name, 'wb') as fp:
+        pickle.dump(df, fp)
+    nr += 1
 
 # Load model and tokenizer
 model_name = 'bert-base-cased'
@@ -63,8 +75,14 @@ model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
 # Make predictions with pipeline
 classifier = pipeline('text-classification', model = model, return_all_scores = True, tokenizer = tokenizer, function_to_apply = 'sigmoid')
-prediction = classifier(df_inf['text'])
+#prediction = classifier(df_inf['text'])
+
+prediction = []
+for data in df_wo78_split:
+    py_data = Dataset.from_pandas(data)
+    preds = classifier(py_data['text'])
+    prediction.append(preds)
 
 # Save predictions
-with open('predictions_8k_v5.pkl', 'wb') as fp:
+with open('predictions_8k.pkl', 'wb') as fp:
     pickle.dump(prediction, fp)
